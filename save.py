822e1d02cdbf4f225cf53ba71019699b2a5e6b26
Lukasz Pustelnik <lpustelnik@zadlo.capdnet>
2020-11-20 13:45:18 +0100

    generate prototypes on mnist

--- save.py
+++ save.py
@@ -1,11 +1,15 @@
 import os
 import torch
 
+best_acc = 0
+
 def save_model_w_condition(model, model_dir, model_name, accu, target_accu, log=print):
     '''
     model: this is not the multigpu model
     '''
-    if accu > target_accu:
+    global best_acc
+    if accu > best_acc and accu > target_accu:
+        best_acc = accu
         log('\tabove {0:.2f}%'.format(target_accu * 100))
         # torch.save(obj=model.state_dict(), f=os.path.join(model_dir, (model_name + '{0:.4f}.pth').format(accu)))
         torch.save(obj=model, f=os.path.join(model_dir, (model_name + '{0:.4f}.pth').format(accu)))
