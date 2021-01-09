b9bd9b398fd2ed29ce202a5050fc8359e4342cbb
Lukasz Pustelnik <lpustelnik@zadlo.capdnet>
2021-01-09 16:28:52 +0100

    update colors

--- plot_logs.py
+++ plot_logs.py
@@ -28,10 +28,9 @@ def plot_conf_matrix(conf_matrix, epoch, path):
     fig = plt.figure()
     conf_matrix = np.array(conf_matrix)
     normlized = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
-    plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.imshow(normlized, interpolation='nearest', cmap=plt.cm.Blues, vmin=0, vmax=1)
     
     thresh = normlized.max() / 2.
-    thresh2 = conf_matrix.max() / 2.
     for i, j in product(range(conf_matrix.shape[0]), range(conf_matrix.shape[1])):
         plt.text(j, i, format(normlized[i, j], '.6f')+"%",
                     verticalalignment="top",
@@ -40,7 +39,7 @@ def plot_conf_matrix(conf_matrix, epoch, path):
         plt.text(j, i, format(conf_matrix[i, j], 'd'),
                     horizontalalignment="center",
                     verticalalignment="bottom",
-                    color="white" if conf_matrix[i, j] > thresh2 else "black")
+                    color="white" if normlized[i, j] > thresh else "black")
     
     plt.xticks([])
     plt.yticks([])
@@ -54,22 +53,6 @@ def plot_conf_matrix(conf_matrix, epoch, path):
     fig.savefig(path+str(epoch)+'_conf_matrix.png', dpi=fig.dpi)
 
 
-def create_conf_matrix():
-    TP, FP = 0, 0
-    FN, TN = 0, 0
-    for inputs, targets in test_loader:
-        inputs, targets = inputs.cuda(), targets.cuda()
-        outputs, _ = ppnet(inputs)
-        predicted = torch.argmax(outputs, dim=1)
-
-        TP += (predicted == targets == 1).sum().item()
-        FP += (predicted == 1 and predicted != targets).sum().item()
-
-        TN += (predicted == targets == 0).sum().item()
-        FN += (predicted == 0 and predicted != targets).sum().item()
-        
-    plot_conf_matrix([[TP, FP], [FN, TN]], 1, "")
-
 if __name__ == "__main__":
     test_acc = [0.5, 0.6, 0.7, 0.8, 0.9]
     test_cross_ent = [0.5, 0.4, 0.3, 0.2, 0.1]
@@ -78,18 +61,7 @@ if __name__ == "__main__":
     test_total_loss = [1.8, 1.5, 1.2, 0.9, 0.6]
 
     X = [[20, 1], [2, 22]]
-    X = [[313, 1], [0, 281]]
-    # plot_conf_matrix(X, 1, "")
-
-    TP, FP = 0, 0
-    FN, TN = 0, 0
-    targets = Tensor([0,1,0,1])
-    predicted = Tensor([1,1,0,1])
-    TP += ((predicted == 1) & (targets == 1)).sum().item()
-    FP += (predicted == 1 & predicted != targets).sum().item()
-    TN += ((predicted == 0) & (targets == 0)).sum().item()
-    # FN += (predicted == 0 and predicted != targets).sum().item()
-
-    print(np.array([[TP, FP], [FN, TN]]))
+    X = [[265, 268], [235, 232]]
+    plot_conf_matrix(X, 1, "")
 
     # plot_logs("10test_", test_acc, test_total_loss, test_cross_ent, test_cluster_cost, test_sep_cost)
