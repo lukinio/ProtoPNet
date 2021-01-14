976f23110c40701263f4151f24ae1324257fc762
Adam Pardyl <adam@pardyl.com>
2021-01-14 21:32:07 +0100

    Add CC 9333

---
+++ saved_models/resnet18_small/026/settings.py
@@ -0,0 +1,43 @@
+base_architecture = 'resnet18_small'
+img_size = 27
+prototype_shape = (20, 512, 2, 2)
+num_classes = 2
+prototype_activation_function = 'log'
+add_on_layers_type = 'regular'
+
+experiment_run = '026'
+
+data_path = 'data/'
+train_dir = data_path + 'train_cropped_augmented/'
+test_dir = data_path + 'bagged_mnist/test/'
+train_push_dir = data_path + 'train_cropped/'
+train_batch_size = 1
+test_batch_size = 1
+train_push_batch_size = 1
+
+joint_optimizer_lrs = {
+    'features': 1e-3,
+    'add_on_layers': 3e-3,
+    'prototype_vectors': 3e-3
+}
+joint_lr_step_size = 5
+
+warm_optimizer_lrs = {
+    'add_on_layers': 3e-3,
+    'prototype_vectors': 3e-3
+}
+
+last_layer_optimizer_lr = 1e-3
+
+coefs = {
+    'crs_ent': 1,
+    'clst': 0.8,
+    'sep': -0.08,
+    'l1': 1e-4,
+}
+
+num_train_epochs = 36
+num_warm_epochs = 10
+
+push_start = 10
+push_epochs = [i for i in range(num_train_epochs) if i % 5 == 0]
--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ coefs = {
 }
 
 num_train_epochs = 36
-num_warm_epochs = 3
+num_warm_epochs = 5
 
 push_start = 10
 push_epochs = [i for i in range(num_train_epochs) if i % 5 == 0]
